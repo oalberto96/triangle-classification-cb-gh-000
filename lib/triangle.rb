92e912f5b2bd638ee326f1f86bc1257ccc677875
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-02 23:20:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,7 @@ class Triangle
 
   def fulfill_inequality_theorem?
     (@side_a + @side_b) > @side_c || (@side_a + @side_c) > @side_b || (@side_b + @side_c) > @side_a
+    binding.pry
   end
 
   def kind
