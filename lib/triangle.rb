d1ed49196ed1157804a9f8220f26df6d18a6f198
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-02 20:21:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,13 +2,17 @@
 
 class Triangle
   attr_accessor :side_a, :side_b, :side_c
-  
+
   def intialize(side_a,side_b,side_c)
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c
   end
-  
+
+  def kind
+    :isosceles
+  end
+
   class TriangleError < StandardError
     def message
       ""
