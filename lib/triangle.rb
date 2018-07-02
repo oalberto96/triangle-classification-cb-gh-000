7e87d82061625ad6e1dc1e9e911c429483aee9ee
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-02 23:18:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,15 +10,16 @@ class Triangle
   end
 
   def greater_than_zero?
-    side_a > 0 && side_b > 0 && side_c > 0
+    @side_a > 0 && @side_b > 0 && @side_c > 0
   end
 
   def fulfill_inequality_theorem?
-    side_a + side_b > side_c || side_a + side_c > side_b || side_b + side_c > side_a
+    @side_a + @side_b > @side_c || @side_a + @side_c > @side_b || @side_b + @side_c > @side_a
   end
 
   def kind
     if greater_than_zero? == false && fulfill_inequality_theorem? == false
+
       raise TriangleError
     else
       if side_a * 3 == side_a + side_b + side_c
