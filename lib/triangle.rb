ea94a26903171286e9aca26678881e0afdeedfc0
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-02 20:24:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
 
   def kind
-    if side_a == side_b && side_a == side_c && side_b == side_c
+    if side_a == side_b == side_c 
       return :equilateral
     else
       return :isosceles
