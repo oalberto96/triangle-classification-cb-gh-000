e21062a7551fdb6d56aa79479873d5910ba63ca6
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-02 20:39:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def kind
     if side_a * 3 == side_a + side_b + side_c
       return :equilateral
-    elsif side_a == side_b && side_c == side_a || side_c == side_a && side_c == side_b
+    elsif side_a == side_b && side_c == side_a || side_c == side_a && side_c == side_b || side_b == side_a && side_b == side_c
       return :isosceles
     end
   end
